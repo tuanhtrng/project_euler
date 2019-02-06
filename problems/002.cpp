df572671f1ad7f00e45c52c43b7fcf50dfce48f5
Tuan H. Truong <tuan.htrng.dev@outlook.com>
2019-02-07 02:58:47 +0700

    better code

--- problems/002.cpp
+++ problems/002.cpp
@@ -2,19 +2,16 @@
 
 auto main() -> int
 {
-    auto fibonacci = int{1};
-    auto fibonacci_next = int{2};
-    auto sum = int{0};
+    auto fibonacci = int{2};
+    auto fibonacci_next = int{8};
+    auto sum = int{fibonacci};
 
     while (fibonacci_next <= 4000000)
     {
-        if (fibonacci_next % 2 == 0)
-        {
-            sum += fibonacci_next;
-        }
+        sum += fibonacci_next;
         auto tmp = fibonacci;
         fibonacci = fibonacci_next;
-        fibonacci_next = fibonacci_next + tmp;
+        fibonacci_next = 4*fibonacci_next + tmp;
     }
     std::cout << sum << "\n";
     return 0;
