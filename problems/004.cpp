be6c6796e0b7369ced46ee427786b4a52926be7e
Tuan H. Truong <tuan.htrng.dev@outlook.com>
2019-02-10 22:03:12 +0700

    better structure demonstrating the problem and its solution

--- problems/004.cpp
+++ problems/004.cpp
@@ -1,30 +1,36 @@
+#include<array>
 #include<cmath>
 #include<iostream>
 
 auto main() -> int
 {
-    // let's say the satisfied palindromic number can be presented as xyzzyx,
-    // where x = [1, 9], y = [1, 9], and z = [0, 9].
-    for (auto x = int{9}; x >= 0; --x)
-    {
-        for (auto y = int{9}; y >= 0; --y)
-        {
-            for (auto z = int{9}; z >= 0; --z)
-            {
-                auto palindromic_number = (
-                    x*100000 + y*10000 + z*1000 + z*100 + y*10 + x
-                );
-                for 
-                (
+// let's say a palindromic number has a form xyzzyx
+    auto const palindromic_number_xyz = [](
+        std::size_t x, std::size_t y, std::size_t z
+    ){
+        return x*100000 + y*10000 + z*1000 + z*100 + y*10 +x;
+    };
+// where x = (0, 9], y = [0, 9], z = [0, 9]
+    auto const nine_to_one = std::array<std::size_t, 9>{
+        9, 8, 7, 6, 5, 4, 3, 2, 1
+    };
+    auto const nine_to_zero = std::array<std::size_t, 10>{
+        9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+    };
+// search for the largest palindromic number that has two 3-digit factors
+    for (auto const & x : nine_to_one){
+        for (auto const & y : nine_to_zero){
+            for (auto const & z : nine_to_zero){
+// construct the defined palindromic number 
+                auto palindromic_number = palindromic_number_xyz(x, y, z);
+// search for satisfied factors
+                for (
                     auto i = std::size_t{100};
                     i <= std::sqrt(palindromic_number);
                     ++i
-                )
-                {
-                    if (palindromic_number%i == 0)
-                    {
-                        if (palindromic_number/i < 1000)
-                        {
+                ){
+                    if (palindromic_number%i == 0){
+                        if (palindromic_number/i < 1000){
                             std::cout << palindromic_number << "\n";
                             return 0;
                         }
